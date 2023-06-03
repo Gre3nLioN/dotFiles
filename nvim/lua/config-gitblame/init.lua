d60c4ac1035fad23e4fd483799bd31406339489b
Ivan Ro <Gre3nLioN@users.noreply.github.com>
2023-06-03 12:21:20 +0100

    Delete nvim/lua directory

--- nvim/lua/config-gitblame/init.lua
+++
@@ -1,5 +0,0 @@
-local g       = vim.g
-local cmd     = vim.cmd
-
-g.gitblame_enabled = 0
-cmd('highlight default link gitblame SpecialComment')
--- nvim/lua/config-lspconfig/flow.lua
+++
@@ -1,8 +0,0 @@
-local lsp_config = require('lspconfig')
-local on_attach = require('config-lspconfig.on_attach')
-
-lsp_config.flow.setup({
-  on_attach = on_attach,
-  init_options = {documentFormatting = false, codeAction = true},
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx" },
-})
